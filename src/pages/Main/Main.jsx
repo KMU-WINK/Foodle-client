52518f0842079e6b397741f7e21202f0a9cd5a4c
jangsumi <aboutimus@gmail.com>
2022-11-16 00:29:13 +0900

    feat: Main 페이지 구현
    - Main 페이지 구현

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Box1, Logo, Magnifier, SearchBar } from './styles';
 
 const Main = () => {
     return (
-        <div>
-            Main
-        </div>
+        <Box1>
+            <Logo>
+                FOODLE
+            </Logo>
+            <SearchBar>
+                원하는 음식의 느낌을 적어보세요.
+                <Magnifier />
+            </SearchBar>
+        
+        </Box1>
     );
 };
 
