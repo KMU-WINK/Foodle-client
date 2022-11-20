66f65ed13d261e7258d7d5dcbfe273334628442a
jangsumi <aboutimus@gmail.com>
2022-11-20 20:46:33 +0900

    refactor: Search 페이지 radio 버튼 리팩터링

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,66 +1,82 @@
-import React, { useState } from 'react';
-import { Box1, BtnContent, BtnSearch, FlexBox, Index, Input, Logo, TitleWant, Title, BtnBox } from './styles';
-import { useNavigate } from 'react-router-dom';
-
-
+import React, { useState } from "react";
+import {
+  Box1,
+  BtnContent,
+  BtnSearch,
+  FlexBox,
+  Index,
+  Input,
+  Logo,
+  TitleWant,
+  Title,
+  BtnBox,
+} from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const GotoMain = () => {
+    navigate("/");
+  };
 
-    const GotoMain = () => {
-        navigate('/');
-    };
+  const [currChecked, setCurrChecked] = useState(0);
+  const [prevChecked, setPrevChecked] = useState(0);
 
-    const [currChecked, setCurrChecked] = useState(0);
-    const [prevChecked, setPrevChecked] = useState(0);
+  function click(index) {
+    setPrevChecked(currChecked);
+    setCurrChecked(index);
+  }
 
-    function click(index) {
-        setPrevChecked(currChecked);
-        setCurrChecked(index);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  const categorySoup = [
+    { index: 1, content: "국물 있음" },
+    { index: 2, content: "국물 없음" },
+    { index: 3, content: "상관 없음" },
+  ];
+
+  function Category(props) {
+    const isOpen = props.isOpen;
+
+    if (isOpen) {
+      return (
+        <>
+          <Index>국물</Index>
+          <BtnBox>
+            {categorySoup.map((soup) => (
+              <BtnContent
+                onClick={() => click(soup.index)}
+                flag={currChecked === soup.index}
+              >
+                {soup.content}
+              </BtnContent>
+            ))}
+          </BtnBox>
+        </>
+      );
     }
+  }
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleOpen = () => {
-        setIsOpen(isOpen => !isOpen);
-    }
-    
-    function Category(props) {
-        const isOpen = props.isOpen;
-    
-        if (isOpen) {
-            return (
-                <>
-                    <Index>국물</Index>
-                    <BtnBox>
-                        <BtnContent onClick={()=>click(1)} flag={currChecked === 1}>국물 있음</BtnContent>
-                        <BtnContent onClick={()=>click(2)} flag={currChecked === 2}>국물 없음</BtnContent>
-                        <BtnContent onClick={()=>click(3)} flag={currChecked === 3}>상관 없음</BtnContent>
-                    </BtnBox>
-                </>
-            );
-        }
-    }
-
-
-
-    return (
-        <FlexBox>
-            <Box1>
-                <Logo onClick={GotoMain}>FOODLE</Logo>
-                <TitleWant fontWeight="500">어떤 느낌의</TitleWant>
-                <TitleWant fontWeight="400">음식을 원하세요?</TitleWant>
-                <Input placeholder='ex. 약간 맵고 달달한 음식'></Input>
-                <Title>먹기 싫은 음식</Title>
-                <Input placeholder='ex. 떡볶이'/>
-                <Title onClick={()=>toggleOpen()}>카테고리</Title>
-                <Category isOpen={isOpen} />
-                <BtnSearch>검색하기</BtnSearch>
-            </Box1>
-        </FlexBox>
-
-    );
+  return (
+    <FlexBox>
+      <Box1>
+        <Logo onClick={GotoMain}>FOODLE</Logo>
+        <TitleWant fontWeight="500">어떤 느낌의</TitleWant>
+        <TitleWant fontWeight="400">음식을 원하세요?</TitleWant>
+        <Input placeholder="ex. 약간 맵고 달달한 음식"></Input>
+        <Title>먹기 싫은 음식</Title>
+        <Input placeholder="ex. 떡볶이" />
+        <Title onClick={() => toggleOpen()}>카테고리</Title>
+        <Category isOpen={isOpen} />
+        <BtnSearch>검색하기</BtnSearch>
+      </Box1>
+    </FlexBox>
+  );
 };
 
 export default Search;
