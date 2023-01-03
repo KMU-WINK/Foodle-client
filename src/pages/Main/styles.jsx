da346ea60bfa28b9a1bb129834da55e802296aa6
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-03 19:43:36 +0900

    style: 메인 화면 styled-components  변경 (반응형 적용)
    - 모바일 환경(440px 이하)에서 검색창이 잘려 보이던 현상 수정
    - max-width, min-width, vw를 이용하여 사용자의 width에 맞추어 검색창의 크기가 변하도록 구현

--- src/pages/Main/styles.jsx
+++ src/pages/Main/styles.jsx
@@ -13,23 +13,23 @@ export const Logo = styled.div`
   font-family: "Noto Sans CJK KR";
   font-style: normal;
   font-weight: 900;
-  font-size: 90px;
-  line-height: 74px;
+  font-size: 72px;
   display: flex;
   align-items: center;
   text-align: center;
   color: #005eeb;
-  margin: 0px 0px 30px 0px;
+  margin: 0px 0px 28px 0px;
 `;
 
 export const SearchBar = styled.div`
-  width: 696px;
-  height: 70px;
+  width: 44vw;
+  min-width: 337px;
+  max-width: 561px;
+  height: 44px;
   font-family: "Noto Sans CJK KR";
   font-style: normal;
   font-weight: 350;
-  font-size: 22px;
-  line-height: 21px;
+  font-size: inherit;
   color: #999999;
   background: #ffffff;
   box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.1);
@@ -37,8 +37,14 @@ export const SearchBar = styled.div`
   display: flex;
   align-items: center;
   text-align: center;
-  padding: 0px 40px 0px 40px;
+  padding: 4px 40px 4px 40px;
   justify-content: space-between;
+  margin-bottom: 32px;
+
+  @media screen and (max-width: 440px) {
+    width: 70px;
+    padding: 4px 20px;
+  }
 `;
 
 export const Magnifier = styled.img.attrs({
