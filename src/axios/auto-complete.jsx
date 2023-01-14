23be5c6cfc1efcb00da6df4e67e01e432ba01d7e
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-14 18:38:22 +0900

    fix: post의 response 데이터가 return 되지 않던 문제 해결

--- src/axios/auto-complete.jsx
+++ src/axios/auto-complete.jsx
@@ -2,10 +2,11 @@ import { autoCompleteService } from "./base-service";
 
 export const getSearchMenus = async (body) => {
   try {
-    autoCompleteService.post(`/api/menus/menu-list`, body).then((response) => {
-      console.log(response);
-      return response;
-    });
+    return autoCompleteService
+      .post(`/api/menus/menu-list`, body)
+      .then((response) => {
+        return response.data;
+      });
   } catch (e) {
     console.log("!", e);
   }
