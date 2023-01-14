c5488f975d1fdbad5d375dcf24dd42078633eb29
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-14 21:32:51 +0900

    feat: 올바르지 않은 경로 접근 Redirect 설정
    - 존재하지 않는 url에 접근하는 경우, 메인 페이지로 redirect(Navigate 사용)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './pages/Main/Main';
-import Search from './pages/Search/Search';
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main/Main";
+import Search from "./pages/Search/Search";
 import Loading from "./pages/Loading/Loading";
 import Result from "./pages/Result/Result";
 
@@ -9,10 +9,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main/>} />
-        <Route path='/search' element={<Search/>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/loading" element={<Loading />} />
         <Route path="/result" element={<Result />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
