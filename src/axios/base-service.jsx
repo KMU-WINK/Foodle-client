02daa5d2891d2d48c10b1805c68abed3cf20591d
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-14 16:40:04 +0900

    feat: axios 초기 설정 추가(검색 자동완성 기능)

--- src/axios/base-service.jsx
+++ src/axios/base-service.jsx
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const baseURL = new URL("http://15.164.0.82:8000/");
 
+const autoCompleteURL = new URL("http://43.200.252.58:8080");
+
 export const baseService = axios.create({
   baseURL: baseURL.toString(),
+  "Content-Type": "application/json",
+});
+
+export const AutoCompleteService = axios.create({
+  baseURL: autoCompleteURL.toString(),
   "Content-Type": "application/json",
 });
 
