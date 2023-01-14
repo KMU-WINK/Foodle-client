9db22de46b2346beb6b91d8155a5ba50e61766ec
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-14 19:02:09 +0900

    hotfix: Typing 라이브러리 제거

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import * as styled from "./styles";
 import { useNavigate } from "react-router-dom";
-import Typing from "react-kr-typing-anim";
 
 const Main = () => {
   const navigate = useNavigate();
-  const Typing = Typing.default ? Typing.default : Typing;
 
   const GotoSearch = () => {
     navigate("/search");
@@ -15,9 +13,7 @@ const Main = () => {
     <styled.Box1>
       <styled.Logo>FOODLE</styled.Logo>
       <styled.SearchBar onClick={GotoSearch}>
-        <Typing Tag="span" preDelay={500} postDelay={100} cursor fixedWidth>
           원하는 음식의 느낌을 적어보세요.
-        </Typing>
         <styled.Magnifier />
       </styled.SearchBar>
     </styled.Box1>
