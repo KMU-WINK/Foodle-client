67aa7105ec4ebf35e481d703b14962db1a3aee01
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-01-14 16:29:44 +0900

    fix: CORS 에러 해결

--- src/axios/base-service.jsx
+++ src/axios/base-service.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const baseURL = new URL('http://15.164.0.82:8000/');
+const baseURL = new URL("http://15.164.0.82:8000/");
 
 export const baseService = axios.create({
-    baseURL: baseURL.toString(),
-    'Content-Type': 'application/json',
-    withCredentials: true,
-    
-})
+  baseURL: baseURL.toString(),
+  "Content-Type": "application/json",
+});
 
 export default baseService;
